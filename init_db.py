ad3654c6263c5eb5c3dca2e446ac6af70e1302c2
Craig Mitchell <cjmitchell347@gmail.com>
2022-06-25 18:47:50 -0500

    created schema.sql and init_db.py, next need to modify to create records in DB

--- init_db.py
+++ init_db.py
@@ -9,12 +9,12 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 # cur.execute("INSERT INTO users (lastname, firstname) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
+#            ('First Post', 'Content for the first post')
+#            )
 
 # cur.execute("INSERT INTO users (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+#            ('Second Post', 'Content for the second post')
+#            )
 
 connection.commit()
 connection.close()
